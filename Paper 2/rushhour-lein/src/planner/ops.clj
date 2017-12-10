bf5d362e0190d51ca8d60fc4ed11af380b5c0601
Jo Jerrica Decker <talktome@diskseven.com>
2017-12-10 18:16:08 +0000

    A few steps closer to nailing the planner approach...

--- Paper 2/rushhour-lein/src/planner/ops.clj
+++ Paper 2/rushhour-lein/src/planner/ops.clj
@@ -4,47 +4,81 @@
 
 (def primary-ops
   '{
-    :mark-horiz
+    :move-horiz
     {
-     :name mark-horiz
+     :name move-horiz
      :achieves (at ?x ?y ?v)
      :when (
             (at ?x ?y none)
             (at ?nx ?y ?_)
-            (at ?vx ?y ?v)
+            (at ?vx1 ?y ?v)
+            (at ?vx2 ?y ?v)
             (isa ?v vehicle)
             (:guard (and
-                     (> (abs (- (? x) (? vx)))
-                        (abs (- (? nx) (? vx))))
-                     (= 1 (abs (- (? x) (? nx)))))))
+                      (= 1 (abs (- (? vx1) (? vx2))))
+                      (> (abs (- (? x) (? vx2)))
+                         (abs (- (? x) (? vx1))))
+                      (> (abs (- (? x) (? vx1)))
+                         (abs (- (? nx) (? vx1))))
+                      (= 1 (abs (- (? x) (? nx)))))))
      :post ((at ?nx ?y ?v))
      :pre ()
-     :del ()
-     :add (clear ?x ?y ?v)
-     :cmd ()
+     :del (
+           (at ?vx2 ?y ?v)
+           )
+     :add (
+           (at ?x ?y ?v)
+           )
+     :cmd ((move ?v ?x ?y))
      :txt ((?x ?y cleared for ?v))
      }
 
 
-    :mark-vert
+    :move-vert
     {
-     :name mark-vert
+     :name move-vert
      :achieves (at ?x ?y ?v)
      :when (
             (at ?x ?y none)
             (at ?x ?ny ?_)
-            (at ?x ?vy ?v)
+            (at ?x ?vy1 ?v)
+            (at ?x ?vy2 ?v)
             (isa ?v vehicle)
             (:guard (and
-                     (> (abs (- (? y) (? vy)))
-                        (abs (- (? ny) (? vy))))
-                     (= 1 (abs (- (? y) (? ny)))))))
+                      (= 1 (abs (- (? vy1) (? vy2))))
+                      (> (abs (- (? x) (? vy2)))
+                         (abs (- (? x) (? vy1))))
+                      (> (abs (- (? y) (? vy1)))
+                         (abs (- (? ny) (? vy1))))
+                      (= 1 (abs (- (? y) (? ny)))))))
      :post ((at ?x ?ny ?v))
      :pre ()
-     :del ()
-     :add (clear ?x ?y ?v)
-     :cmd ()
+     :del (
+           (at ?x ?vy2 ?v)
+           )
+     :add (
+           (at ?x ?y ?v)
+           )
+     :cmd ((move ?v ?x ?y))
      :txt ((?x ?y cleared for ?v))
+     }
+
+    :branch-horiz
+    {
+     :name branch-horiz
+     :achieves (at ?x ?y ?tv)
+     :when ((at ?x ?y ?av)
+            (isa ?av vehicle)
+            (orientation ?av horiz)
+            (max-size ?sx ?sy))
+     :post ((at ?sx ?y ?av)
+            (at 0 ?y ?av)
+            (at ?x ?y ?tv))
+     :pre ()
+     :del ()
+     :add ()
+     :cmd ()
+     :txt ()
      }
 
     :branch-vert
@@ -56,7 +90,8 @@
             (orientation ?av vert)
             (max-size ?sx ?sy))
      :post ((at ?x ?sy ?av)
-            (at ?x 0 ?av))
+            (at ?x 0 ?av)
+            (at ?x ?y ?tv))
      :pre ()
      :del ()
      :add ()
